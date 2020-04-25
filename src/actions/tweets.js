9583852e2574d1bc459371aa420292b1fd57bb25
marynapl <kushnirmarinka@gmail.com>
2020-04-24 20:52:23 -0400

    Add New Tweet component

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,16 @@
-import { saveLikeToggle } from '../utils/api'
+import { saveLikeToggle, saveTweet } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
+
+export function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  }
+}
 
 export function receiveTweets(tweets) {
   return {
@@ -16,6 +25,21 @@ function toggleTweet({ id, authedUser, hasLiked }) {
     id,
     authedUser,
     hasLiked
+  }
+}
+
+export function handleAddTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+    dispatch(showLoading())
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()))
   }
 }
 
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,15 +1,16 @@
-import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_TWEET, ADD_TWEET } from '../actions/tweets'
 
 export default function tweets(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
-      return { 
-        ...state, 
-        ...action.tweets }
+      return {
+        ...state,
+        ...action.tweets
+      }
 
     case TOGGLE_TWEET:
-      return { 
-        ...state, 
+      return {
+        ...state,
         [action.id]: {
           ...state[action.id],
           likes: action.hasLiked === true
@@ -18,6 +19,24 @@ export default function tweets(state = {}, action) {
         }
       }
 
+    case ADD_TWEET:
+      const { tweet } = action
+
+      let replyingTo = {}
+      if (tweet.replyingTo !== null) {
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyingTo].replies.concat([tweet.id])
+          }
+        }
+      }
+
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet,
+        ...replyingTo,
+      }
     default:
       return state
   }
